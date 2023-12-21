6400f774192149137205769a02a2e23b761982d2
Muhammad Hassan Thalib <muhammadhassan7788@gmail.com>
2023-12-21 19:54:28 +0700

    simplify column

--- src/main/kotlin/com/muhammhassan/repository/database/scheme/Target.kt
+++ src/main/kotlin/com/muhammhassan/repository/database/scheme/Target.kt
@@ -1,13 +1,14 @@
 package com.muhammhassan.repository.database.scheme
 
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.datetime
-import org.jetbrains.exposed.sql.javatime.timestamp
 
-object Target: Table("target"){
+object Target : Table("target") {
     val id = integer("id").autoIncrement()
-    val month = integer("month")
-    val createdAt = datetime("created_at")
+    val time = varchar("time", 7).uniqueIndex()
+    val target = integer("target")
+    val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
 
     override val primaryKey: PrimaryKey
         get() = PrimaryKey(id)
