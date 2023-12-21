4d83f2f3965ad3c63c4647fbf09a970010833b40
Muhammad Hassan Thalib <muhammadhassan7788@gmail.com>
2023-12-21 15:35:14 +0700

    Update cred security

--- src/main/kotlin/com/muhammhassan/plugins/DatabaseConfig.kt
+++ src/main/kotlin/com/muhammhassan/plugins/DatabaseConfig.kt
@@ -6,10 +6,8 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseConfig {
-    fun init(){
+    fun init(username: String, password: String){
         val driver = "com.mysql.cj.jdbc.Driver"
-        val username="wm69287bns8agrlkt2zb"
-        val password = "pscale_pw_El8WAYfMozg8vcPFO9JxEZanyIMZqCb5tiPOFCCTraY"
         val url = "jdbc:mysql://aws.connect.psdb.cloud/review-performance-monitor?sslMode=VERIFY_IDENTITY"
         Database.connect(url, driver, username, password)
 
