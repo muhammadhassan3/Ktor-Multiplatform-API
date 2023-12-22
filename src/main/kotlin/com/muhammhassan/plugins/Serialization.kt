528384fe617939f827f10ba737038dd0ace4866f
Muhammad Hassan Thalib <muhammadhassan7788@gmail.com>
2023-12-22 21:04:55 +0700

    Integrate LocalDateTime gson converter

--- src/main/kotlin/com/muhammhassan/plugins/Serialization.kt
+++ src/main/kotlin/com/muhammhassan/plugins/Serialization.kt
@@ -1,19 +1,22 @@
 package com.muhammhassan.plugins
 
+import com.muhammhassan.utils.LocalDateTimeAdapter
 import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.time.LocalDateTime
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         gson {
-            }
+            registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeAdapter())
+        }
     }
     routing {
         get("/json/gson") {
-                call.respond(mapOf("hello" to "world"))
-            }
+            call.respond(mapOf("hello" to "world"))
+        }
     }
 }
