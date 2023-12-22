70265dd057f97311f41cb2b75cc0153e183f7f7a
Muhammad Hassan Thalib <muhammadhassan7788@gmail.com>
2023-12-22 19:54:39 +0700

    Simplify entity superclass

--- src/main/kotlin/com/muhammhassan/repository/database/scheme/Target.kt
+++ src/main/kotlin/com/muhammhassan/repository/database/scheme/Target.kt
@@ -1,15 +1,11 @@
 package com.muhammhassan.repository.database.scheme
 
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.datetime
 
-object Target : Table("target") {
-    val id = integer("id").autoIncrement()
+object Target : IntIdTable("target") {
     val time = varchar("time", 7).uniqueIndex()
     val target = integer("target")
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
-
-    override val primaryKey: PrimaryKey
-        get() = PrimaryKey(id)
 }
