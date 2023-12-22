a1b0e6512b93196b9c98f0cfa16a2ba72674d25a
Muhammad Hassan Thalib <muhammadhassan7788@gmail.com>
2023-12-22 21:28:08 +0700

    Centralized routing method

--- src/main/kotlin/com/muhammhassan/plugins/Routing.kt
+++ src/main/kotlin/com/muhammhassan/plugins/Routing.kt
@@ -1,13 +1,12 @@
 package com.muhammhassan.plugins
 
+import com.muhammhassan.route.registerPerformanceRoute
+import com.muhammhassan.route.registerTargetRoute
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respond(mapOf("error" to false))
-        }
-    }
+    registerTargetRoute()
+    registerPerformanceRoute()
 }
