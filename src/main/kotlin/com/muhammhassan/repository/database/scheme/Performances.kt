6b511addc416912e48bcb6cf023fead269084267
Muhammad Hassan Thalib <muhammadhassan7788@gmail.com>
2023-12-22 21:11:14 +0700

    Integrate DAO

--- src/main/kotlin/com/muhammhassan/repository/database/scheme/Performances.kt
+++ src/main/kotlin/com/muhammhassan/repository/database/scheme/Performances.kt
@@ -1,10 +1,20 @@
 package com.muhammhassan.repository.database.scheme
 
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.datetime
+import java.time.LocalDateTime
 
 object Performances : IntIdTable("performance") {
     val point = integer("point")
-    val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
+    val createdAt = datetime("created_at").clientDefault { LocalDateTime.now() }
+}
+
+class Performance(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Performance>(Performances)
+
+    var point by Performances.point
+    val createdAt by Performances.createdAt
 }
