5727dfd7d5a32714f00e0a544793dbb6144dee7f
Muhammad Hassan Thalib <muhammadhassan7788@gmail.com>
2023-12-22 21:04:42 +0700

    Refactoring

--- src/main/kotlin/com/muhammhassan/repository/database/scheme/Performances.kt
+++ src/main/kotlin/com/muhammhassan/repository/database/scheme/Performances.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.datetime
 
-object Performance : IntIdTable("performance") {
+object Performances : IntIdTable("performance") {
     val point = integer("point")
     val createdAt = datetime("created_at").defaultExpression(CurrentDateTime)
 }
